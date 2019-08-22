afd53c9e8693d0859b0e84767f98340600b81592
arthur wang <2629917751@qq.com>
2019-08-22 10:22:03 +0800

    更新连接数据库，并对数据库进行操的相关代码。

--- project.py
+++ project.py
@@ -1,10 +1,14 @@
-from flask import Flask,render_template,flash
+from flask import Flask,render_template,flash,redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField,SubmitField,StringField,BooleanField
 from wtforms.validators import DataRequired,Length
+import sqlite3
 
 app = Flask('project')
 app.secret_key = 'nata'
+
+conn = sqlite3.connect("Nata.db",check_same_thread=False)
+db = conn.cursor()
 
 @app.route('/about_me')
 def about_me():
@@ -17,12 +21,35 @@ def index():
 @app.route('/natabuli', methods=['GET','POST'])
 def natabuli():
     form = NataForm()
+    if form.validate_on_submit():
+        body = form.body.data
+        name = form.name.data
+
+        try:
+            db.execute('CREATE TABLE message(content text)')
+
+        except:
+            print("CREATE TABLE message...  Failed!")
+
+        print('INSERT INTO message VALUES ("%s")' % (body))
+        try:
+            db.execute('INSERT INTO message VALUES ("%s")' % (body))
+        except:
+            print("Failed!")
+
+
+        conn.commit()
+        flash('Your message have been sent to the world!')
+
+    else:print("表单验证不通过")
+
+
     return render_template('natabuli/home.html',form=form)
 
 
 class NataForm(FlaskForm):
-    body = TextAreaField(label='messages',validators=[DataRequired(message=u'说点什么吧，哈哈'), Length(1,300)])
-    name = StringField('yourname',validators=[DataRequired(message=u'需要留下你的大名哟，啦啦')])
+    body = TextAreaField(label='messages',validators=[DataRequired()])
+    name = StringField('yourname',validators=[DataRequired()])
     ano = BooleanField('匿名提交')
     submit = SubmitField('submit')
 
